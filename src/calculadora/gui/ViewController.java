8c60d48b757483c58beada6bf9e040416db23126
Fernando <fernando.luiz92@hotmail.com>
2022-02-02 08:07:23 -0300

    ComboBox

--- src/calculadora/gui/ViewController.java
+++ src/calculadora/gui/ViewController.java
@@ -1,63 +1,68 @@
 package calculadora.gui;
 
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
 public class ViewController implements Initializable {
 
+	// Anotação para fazer associação com a interface gráfica
 	@FXML
-	private Button btTest;
+	private ComboBox<Person> comboBoxPerson;
 
 	@FXML
-	private TextField txtNumber1;
-
+	private Button btAll;
+	
+	// ObservableList - tipo especial de lista que já fica associado ao componente
+	// visual, implementa uma interação entre a interface gráfica(componente) e a
+	// coleção(lista)
+	private ObservableList<Person> obsList;
+	
 	@FXML
-	private TextField txtNumber2;
-
+	public void onComboBoxPersonAction() {
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
+	}
+	
 	@FXML
-	private Label labelResult;
-
-	@FXML
-	private Button btSum;
-
-	@FXML
-	public void onBtSumAction() {
-		try {
-			Locale.setDefault(Locale.US);
-			double number1 = Double.parseDouble(txtNumber1.getText());
-			double number2 = Double.parseDouble(txtNumber2.getText());
-			double sum = number1 + number2;
-			labelResult.setText(String.format("%.2f", sum));
-		} catch (NumberFormatException e) {
-			Alerts.showAleart("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+	public void onBtnAllAction() {
+		
+		for(Person person : comboBoxPerson.getItems()) {
+			System.out.println(person);
 		}
 	}
 
-	@FXML
-	public void onBtTestAction() {
-		// Alerts.showAleart("ALERT TITLE", null,"ALERT CONTENT",
-		// AlertType.INFORMATION);
-
-	}
-	/*
-	 * URL - caminho da tela, na maioria das vzes n iremos usar.
-	 * ResourceBundle - recursos que poderá usar na aplicação
-	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		Constraints.setTextFieldDouble(txtNumber1);
-		Constraints.setTextFieldDouble(txtNumber2);
-		Constraints.setTextFieldMaxLength(txtNumber1, 5);
-		Constraints.setTextFieldMaxLength(txtNumber2, 5);
+		List<Person> list = new ArrayList<>();
+		list.add(new Person(1,"Maria","maria@gmail.com"));
+		list.add(new Person(2,"Alex","alex@gmail.com"));
+		list.add(new Person(3,"Bob","bob@gmail.com"));
+		
+		//FXCollections - Classe do JAVAFX
+		obsList = FXCollections.observableArrayList(list); // instancia a obsList
+		comboBoxPerson.setItems(obsList);// define os valores no combobox
+		
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
 	}
 }
